11d1bc416e460532ff6bfc90bf25cf8b4ceaeaba
Alex Pison <pisonalex@gmail.com>
2025-01-02 21:22:01 +0100

    Horse can now be turned 180 and 270 degrees to the right

--- cwk2_24-25.hs
+++ cwk2_24-25.hs
@@ -9,6 +9,9 @@ horse =
     "     / '\\-)"
   ]
 
+text :: [String]
+text = ["abc", "def", "ghi"]
+
 pretty :: Horse -> IO ()
 pretty [] = return ()
 pretty (x : xs) = do
@@ -19,11 +22,16 @@ mirror :: Horse -> Horse
 mirror = map reverse
 
 transposeHelper :: Horse -> Horse
-transposeHelper ([] : _) = []
-transposeHelper x = map head x : transposeHelper (map tail x)
+transposeHelper = foldr (zipWith (:)) (repeat [])
 
 transpose :: Horse -> Horse
 transpose = map reverse . transposeHelper
+
+turn180 :: Horse -> Horse
+turn180 = transpose . transpose
+
+turn270 :: Horse -> Horse
+turn270 = mirror . transpose . mirror
 
 -- 3 --
 nextTrib :: (Num c) => (c, c, c) -> (c, c, c)
